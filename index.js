d963c1b2f134512002c47c9f55cbcde5d516aba7
majhailsid <maskedprogrammeryt@gmail.com>
2023-08-20 12:00:56 +0000

    feat(app): Added Methods such as Search by ID or NAME, ADD new │ │  Added new methods to filer products by using their ID or NAME as an arguments, integrated new method to add a new product and its details. │ │  Method addNewProduct is stil in development phase, while method showProductDetailsById and showProductDetailsByName are finished.

--- index.js
+++ index.js
@@ -1,37 +1,57 @@
 console.log("This is a CLI-Stock Management Application developer by Yashdeep Singh Sidhu");
-console.log(`Github : @majhailsid | LinkedIn : @majhail | Twitter : Yxshdeep_singh | Email : yashdeep280905@gmail.com \n`)
+console.log(`Github : @majhailsid | LinkedIn : @majhail | Twitter : Yxshdeep_singh | Email : yashdeep280905@gmail.com\n`)
 
-let operateVal = prompt("Select what operation you want to operate : \n 1. Add new Item \n 2. Delete Existing Item \n 3. Update Existing Item \n 4. Exit \n");
+// let operateVal = prompt("Select what operation you want to operate : \n 1. Add new Item \n 2. Delete Existing Item \n 3. Update Existing Item \n 4. Exit \n");
 
-// switch (operateVal) {
-//   case "1":
-//     console.log("ADD NEW ITEM :\n");
-//     break;
-//   case "2":
-//     console.log("DELETE EXISTING ITEM :\n");
-//     break;
-//   case "3":
-//     console.log("UPDATE EXISTING ITEM :\n");
-//     break;
-//   case "4":
-//     console.log("EXIT");
-//     break;
-// }
-
-
-if(operateVal <= 0 || operateVal > 4){
-  console.log("Please select a valid option :\n")
-}else{
-  if(operateVal == 1){
-    console.log("ADD NEW ITEM :\n");
-  }else{
-    if(operateVal == 2){
-      console.log("DELETE EXISTING ITEM :\n");
-    }else{
-      if(operateVal == 3){
-        console.log("UPDATE EXISTING ITEM :\n");
+let productDataBase = {
+  productId: [1,2,3,4,5],
+  productName: ["Product1","Product2","Product3","Product4","Product5"],
+  productPrice: [55,53,66,22,67],
+  productPresentQuantity: [5,2,7,3,9],
+  productDetails: ["Product1","Product2","Product3","Product4","Product5"],
+  //------------------------------------------------------
+  showProductDetailsById: function(idNumber) {
+    if(this.productId.length == 0){
+      console.log('There is no product to show! Please Add one!')
+    }else{ let id = Number.parseInt(idNumber);
+    for (let item=0; item < this.productId.length; item++) {
+      if (this.productId[item] === id) {
+        console.log(`\n\tDetails of ${this.productName[item]}\n\n\tProduct Id : ${id}\n\tProduct Name : ${this.productName[item]}\n\tPrice : ${this.productPrice[item]}\n\tQuantity Present(Current) : ${this.productPresentQuantity[item]}\n\tDetails : ${this.productDetails[item]}\n`)
       }
-    }
+    }}
+  },
+  showProductDetailsByName : function(pName){
+    if(this.productId.length == 0){
+      console.log('There is no product to show! Please Add one!')
+    }else{let pName_string = pName.toString();
+    for (let name=0; name < this.productId.length; name++){
+      if(this.productName[name] === pName_string){
+        console.log(`\n\tDetails of ${this.productName[name]}\n\n\tProduct Id : ${name}\n\tProduct Name : ${this.productName[name]}\n\tPrice : ${this.productPrice[name]}\n\tQuantity Present(Current) : ${this.productPresentQuantity[name]}\n\tDetails : ${this.productDetails[name]}\n`)
+      }
+    }}
+  },
+  addNewProduct : function(){
+    let storeNewProductName = prompt("\t\nProduct Name :");
+    let storeNewProductPrice = Number.parseInt(prompt("\tPrice : "));
+    let storeNewProductPresentQuantity = Number.parseInt(prompt("\tPresent Quantity(Current) : "));
+    let storeProductDetails = prompt("\tDetails : ");
   }
-  console.log("Exiting...")
 }
+
+productDataBase.addNewProduct();
+// if(operateVal <= 0 || operateVal > 4){
+//   console.log("Please select a valid option :\n")
+// }else{
+//   if(operateVal == 1){
+//     console.log("ADD NEW ITEM :\n");
+//   }else{
+//     if(operateVal == 2){
+//       console.log("DELETE EXISTING ITEM :\n");
+//     }else{
+//       if(operateVal == 3){
+//         console.log("UPDATE EXISTING ITEM :\n");
+//       }
+//     }
+//   }
+//   console.log("Exiting...")
+// }
